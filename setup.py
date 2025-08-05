cb13c030792a40f3234663bb5ad49fce8e7a31b8
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2025-08-05 10:38:37 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="kececifractals",
-    version="0.1.2",
+    version="0.1.3",
     description="Keçeci Fractals: Keçeci-style circle fractal.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -20,7 +20,7 @@ setup(
         "numpy",
         "matplotlib",
         "networkx",
-        "kececilayout",
+        "kececilayout"
     ],
     extras_require={
 

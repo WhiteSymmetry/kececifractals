43f2606c06f26daeed0c109129919650198f2dde
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2026-01-24 05:28:43 +0300

    Update _version.py

--- kececifractals/_version.py
+++ kececifractals/_version.py
@@ -1,9 +1,9 @@
 # _version.py
 
-__version__ = "0.1.5"
-__license__ = "MIT"
+__version__ = "0.1.6"
+__license__ = "AGPL3.0-or-later"
 __description__ = "Keçeci Fractals: Keçeci-style circle fractal."
 __author__ = "Mehmet Keçeci"
 __url__ = "https://github.com/WhiteSymmetry/kececifractals"
 __docs__ = "https://github.com/WhiteSymmetry/kececifractals"  # Opsiyonel: Dokümantasyon linki
-__dependencies__ = ["python>=3.10"]  # Diğer bağımlılıkları da ekleyebilirsiniz
+__dependencies__ = ["python>=3.11"]  # Diğer bağımlılıkları da ekleyebilirsiniz

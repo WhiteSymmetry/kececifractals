4c24e9ced8b8f601b6108e7b80655e6d71fc98b6
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2026-01-26 00:19:33 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
 # -*- coding: utf-8 -*-
+import io
+import re
 from setuptools import setup, find_packages
+import sys
+import os
 
+# UTF-8 encoding sorunlarını çöz
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+def get_version():
+    with open('kececifractals/__init__.py', 'r', encoding='utf-8') as f:
+        content = f.read()
+    match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", content, re.M)
+    if match:
+        return match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 setup(
     name="kececifractals",
-    version="0.1.9",
+    version=get_version(),
     description="Keçeci Fractals: Keçeci-style circle fractal.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",

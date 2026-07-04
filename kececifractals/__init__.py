0b760ae17c59cf1da0ff4e78b2d8db5634d44981
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2026-07-04 15:39:46 +0300

    Bump version number to 0.2.6

--- kececifractals/__init__.py
+++ kececifractals/__init__.py
@@ -13,7 +13,7 @@ import warnings
 from typing import List, Optional, Tuple, Union, Callable
 
 # Paket sürüm numarası
-__version__ = "0.2.5"
+__version__ = "0.2.6"
 __author__ = "Mehmet Keçeci"
 __email__ = "mkececi@yaani.com"
 __license__ = "AGPL3.0-or-later"
